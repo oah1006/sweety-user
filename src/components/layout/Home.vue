c85fca7e40e4f4a80ab9a4901a23ad60d8908185
oah1006 <bnhao10062001@gmail.com>
2023-04-22 12:07:45 +0700

    update

--- src/components/layout/Home.vue
+++ src/components/layout/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="px-20 py-5">
     <Header />
+    <div class="mt-10 ml-32 text-4xl font-medium">
+      <p>Trà sữa tươi ngon</p>
+      <p class="px-10">đủ để làm hài lòng mọi tâm hồn!</p>
+    </div>
   </div>
 </template>
 
