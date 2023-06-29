f3823f71068d4c16c0bb61e6903941e1c062f8b0
OaH <81689054+oah1006@users.noreply.github.com>
2023-06-30 02:11:58 +0700

    up github

--- src/views/checkout/Checkout.vue
+++ src/views/checkout/Checkout.vue
@@ -21,23 +21,25 @@
             </div>
           </div>
           <div v-if="isDeliveryAddressModal" class="mt-1 border border-zinc-300 rounded-lg">
-            <div v-if="delivery_addresses" v-for="delivery_address in delivery_addresses" :key="delivery_address.id">
-              <div @click="chooseMyDeliveryAddressOrder(delivery_address.id, delivery_address)" :class="[deliveryAddressDefault.id == delivery_address.id ? 'bg-orange-500' : '']"
-                   class="flex items-center gap-3 py-3 px-5 cursor-pointer">
-                <div class="w-1/12">
-                  <svg v-if="deliveryAddressDefault?.id == delivery_address?.id" xmlns="http://www.w3.org/2000/svg"
-                       fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                  </svg>
-                  <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                  </svg>
+            <div v-if="delivery_addresses" >
+              <div v-for="delivery_address in delivery_addresses" :key="delivery_address.id">
+                <div @click="chooseMyDeliveryAddressOrder(delivery_address.id, delivery_address)" :class="[deliveryAddressDefault.id == delivery_address.id ? 'bg-orange-500' : '']"
+                     class="flex items-center gap-3 py-3 px-5 cursor-pointer">
+                  <div class="w-1/12">
+                    <svg v-if="deliveryAddressDefault?.id == delivery_address?.id" xmlns="http://www.w3.org/2000/svg"
+                         fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-white">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                    </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+                    </svg>
+                  </div>
+                  <p :class="[deliveryAddressDefault?.id == delivery_address?.id ? 'text-white' : '']"
+                     class="text-sm">
+                    {{ delivery_address?.street_number }} {{ delivery_address?.street }}, {{ delivery_address?.ward?.name }},
+                    {{ delivery_address?.district?.name }}, {{ delivery_address?.province?.name }}</p>
                 </div>
-                <p :class="[deliveryAddressDefault?.id == delivery_address?.id ? 'text-white' : '']"
-                   class="text-sm">
-                  {{ delivery_address?.street_number }} {{ delivery_address?.street }}, {{ delivery_address?.ward?.name }},
-                  {{ delivery_address?.district?.name }}, {{ delivery_address?.province?.name }}</p>
               </div>
             </div>
             <div v-else class="px-3 py-4 flex flex-col items-center gap-2">
